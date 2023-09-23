ab2472ab05aa9ba6ffd6182a3bc4e7e99d4c006b
Haji <835510106@qq.com>
2023-09-23 12:07:59 +0800

    update

--- main.js
+++ main.js
@@ -1,55 +1,60 @@
-const { app, BrowserWindow, globalShortcut } = require("electron");
+const { app, BrowserWindow, globalShortcut, Menu, MenuItem ,screen} = require("electron");
 // app，它着您应用程序的事件生命周期。
 // BrowserWindow，它负责创建和管理应用窗口。
 
 // createWindow 函数用于创建一个应用窗口
 const createWindow = () => {
+	const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+	const windowWidth = 400
+	const x = width - windowWidth
 	const win = new BrowserWindow({
 		// 设置宽高
-		width: 800,
-		height: 600,
+		width: windowWidth,
+		height: height,
+		x: x,
+		y: 0,
 		titleBarStyle: "customButtonsOnHover",
-        // 失去焦点的时候隐藏窗口
-        // hiddenInMissionControl:true,
-        vibrancy:"sheet",
+		// 失去焦点的时候隐藏窗口
+		// hiddenInMissionControl:true,
+		vibrancy: "sheet",
+		
 	});
 
-	win.once("ready-to-show", () => {
-		win.show();
-	});
-
-	// // 在关闭窗口时，将窗口隐藏而不是关闭
-	// win.on("close", (e) => {
-	// 	e.preventDefault();
-	// 	win.hide();
-	// });
-
-	// 当点击程序坞中的应用图标时，显示窗口
-	app.on("activate", () => {
-		if (win.isVisible() === false) {
-			win.show();
-		}
-	});
-
-	// 注册隐藏窗口的快捷键为Cmd+W
-	const hideWindowShortcut = globalShortcut.register(
-		"CommandOrControl+W",
-		() => {
-			win.hide();
-		}
+	const menu = new Menu();
+	menu.append(
+		new MenuItem({
+			label: "ChatGPT",
+			submenu: [
+				{
+					role: "Quit",
+					accelerator:
+						process.platform === "darwin" ? "Cmd+Q" : "Alt+Shift+Q",
+					click: () => {
+						console.log("Electron rocks!");
+					},
+				},
+				{
+					role: "Hide",
+					accelerator:
+						process.platform === "darwin" ? "Cmd+W" : "Alt+Shift+W",
+					click: () => {
+						console.log("Electron rocks!");
+					},
+				},
+				{
+					role: "Zoom",
+					accelerator:
+						process.platform === "darwin" ? "Cmd+F" : "Alt+Shift+F",
+					click: () => {
+						console.log("Electron rocks!");
+					},
+				},
+			],
+		})
 	);
+	Menu.setApplicationMenu(menu)
 
-	app.on("will-quit", () => {
-		// 解除Cmd+W快捷键的注册
-		globalShortcut.unregister("CommandOrControl+W");
-	});
 
-	app.on("window-all-closed", () => {
-		app.quit(); // 退出应用
-	});
-
-	// 加载静态页面
-	//   win.loadFile('index.html')
 	// 加载网址
 	win.loadURL("https://chat.openai.com");
 };
